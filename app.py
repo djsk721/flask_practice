7e99f178088059d939f16594036b52ae825e1833
unknown <djsk721@naver.com>
2023-02-22 18:06:55 +0900

    app.py 수정2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'plz press the like btn !! by Kim 해치웠나 좀 되라'
+    return '씨발'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
